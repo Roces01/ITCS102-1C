7126f522e7ed7b617c82da0abc968ab107edca6e
Roces01 <johnpaulroces4@gmail.com>
2025-10-14 13:03:44 +0800

    Create Activity20.py

---
+++ Activity20.py
@@ -0,0 +1,11 @@
+WAWA = True
+
+while WAWA == True:
+    a = input("Want to continue(Yes or No)-->").lower()
+    
+    if a == "yes":
+        print("This will continue!!!")
+        continue   
+    else:
+        print("Stop")
+        break
